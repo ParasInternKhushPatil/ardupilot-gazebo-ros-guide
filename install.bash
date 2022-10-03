2eb03df7db6a75d582bd40f0c7ceeddf35a51ad3
yunusaksuoglu <114915305+yunusaksuoglu@users.noreply.github.com>
2022-10-03 12:57:36 +0300

    Update install.bash

--- install.bash
+++ install.bash
@@ -5,8 +5,7 @@ set -x
 DISTRO="melodic"
 IWD="$HOME"
 
-# Preventing sudo timeout https://serverfault.com/a/833888
-trap "exit" INT TERM; trap "kill 0" EXIT; sudo -v || exit $?; sleep 1; while true; do sleep 60; sudo -nv; done 2>/dev/null &
+
 
 # 0 Prerequisites
 
